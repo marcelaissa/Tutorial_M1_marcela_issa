648312232a1b0a7234df722b9806e6c1a86525f4
marcelaissa <marcela.martins@sou.inteli.edu.br>
2023-02-02 11:28:59 -0300

    atualização código flappy dragon

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -2,8 +2,8 @@ extends Node # instancia a classe Node2D
 
 var status = 1 # define a forma como o jogo começará: se =0: o jogo começará pausado e será necessário apertar a tecla 'enter' para iniciar; se x=1: começará imediatamente; se x>1: jogo não irá rodar.
 var vscore = 0 # define de que número iniciará a contagem dos pontos.
-var x = 3 #altera a velocidade horizontal dragão
-var y = 1.5 #altera a gravidade
+var x = 3 #altera a velocidade horizontal dragão.
+var y = 1.5 #altera a gravidade.
 
 # executa essa função ao carregar o jogo
 func _ready():
